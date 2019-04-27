889aa66d4f76197899fafde8b84d8af892e2d561
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-04-27 15:53:27 +0200

    initial commit

---
+++ lib/issue_fetcher.ex
@@ -0,0 +1,18 @@
+defmodule IssueFetcher do
+  @moduledoc """
+  Documentation for IssueFetcher.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IssueFetcher.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/issue_fetcher_test.exs
@@ -0,0 +1,8 @@
+defmodule IssueFetcherTest do
+  use ExUnit.Case
+  doctest IssueFetcher
+
+  test "greets the world" do
+    assert IssueFetcher.hello() == :world
+  end
+end
